e621094e59e6c64e6b5107aaff16bce6d2794a98
Sheehan Alam <sheehan@tappforce.com>
2012-07-13 03:14:36 +0100

    init commit

---
+++ StartupVideos/StartupVideos/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  StartupVideos
+//
+//  Created by Sheehan Alam on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ StartupVideos/StartupVideos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StartupVideos
+//
+//  Created by Sheehan Alam on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
